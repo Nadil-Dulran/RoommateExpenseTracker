ea8f6c2ce5ed6765f212efa2a279fbb341e49772
Nadil-Dulran <it23161788@my.sliit.lk>
2026-02-19 13:24:57 +0530

    Update activity screen with sample structure

--- src/screens/main/ActivityScreen.tsx
+++ src/screens/main/ActivityScreen.tsx
@@ -1,10 +1,38 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
 export default function ActivityScreen() {
+  const navigation = useNavigation<any>();
+
   return (
+        <View style={styles.container}>
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text style={{ fontSize: 24, fontWeight: "bold" }}>Welcome to the Activity Screen!</Text>
+      <View style={{ marginTop: 20 }}>
+           <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Profile')}>
+                <Text style={styles.buttonText}>View Profile</Text>
+              </TouchableOpacity>
+              </View>
+    </View>
     </View>
   );
 }
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#F9FAFB',
+  },
+  button: {
+    marginTop: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#009966',
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: '#a91313',
+    fontSize: 16,
+  },
+};
