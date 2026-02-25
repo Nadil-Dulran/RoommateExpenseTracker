90ecd7633fda9dcb0f481612a0a05bcfc7ac855a
Nadil-Dulran <it23161788@my.sliit.lk>
2026-02-25 21:57:08 +0530

    Update notification types

--- src/types/notification.ts
+++ src/types/notification.ts
@@ -1,4 +1,24 @@
+
+export type CategoryType = 'food' | 'travel' | 'utilities';
+
 export interface Notification {
   id: string;
   read: boolean;
+  type: 'expense_added' | 'expense_settled';
+  date: string;
+  message: string;
+  groupName: string;
+  groupEmoji: string;
+  relatedUser: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
+  expense?: {
+    category: CategoryType;
+    splits: {
+      userId: string;
+      amount: number;
+    }[];
+  };
 }
