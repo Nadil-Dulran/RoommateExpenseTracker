fbdc870ae4a13867202dcf3a1dd474335515353b
Nadil-Dulran <it23161788@my.sliit.lk>
2026-03-05 16:39:36 +0530

    Create authentication service

---
+++ src/services/authService.ts
@@ -0,0 +1,32 @@
+const API_URL = 'http://10.0.2.2:3000/api/auth';
+
+export const authService = {
+
+  register: async (data: any) => {
+
+    const response = await fetch(`${API_URL}/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    return response.json();
+  },
+
+  login: async (data: any) => {
+
+    const response = await fetch(`${API_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    return response.json();
+  },
+
+};
+
