8eff7d7a249db2816160406413920195698aefc5
Nadil-Dulran <it23161788@my.sliit.lk>
2026-03-25 14:11:13 +0530

    Created currency list in separate file

---
+++ src/constants/currencies.ts
@@ -0,0 +1,5 @@
+export const currencies = [
+  { code: 'USD', symbol: '$', name: 'US Dollar' },
+  { code: 'INR', symbol: '₹', name: 'Indian Rupee' },
+  { code: 'LKR', symbol: 'Rs.', name: 'Sri Lankan Rupee' },
+];
