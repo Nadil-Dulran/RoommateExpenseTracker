600423b1c3b8ff660ae6ebf8ac9f7c4a8a41eb57
Nadil-Dulran <it23161788@my.sliit.lk>
2026-03-25 15:07:50 +0530

    Wrap the whole app with the currency provider for update the changes

--- App.tsx
+++ App.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import  AppNavigator  from './src/navigation/AppNavigator';
+import { CurrencyProvider } from './src/context/CurrencyContext';
 
 
 const App = () => {
 
   return (
     <SafeAreaProvider>
+      <CurrencyProvider>
         <NavigationContainer>
           <AppNavigator/>
         </NavigationContainer>
+      </CurrencyProvider>
     </SafeAreaProvider>
   );
 };
