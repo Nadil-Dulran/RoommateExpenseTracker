911089eaa46727f0699084695ec45a65ea70fcb6
Nadil-Dulran <it23161788@my.sliit.lk>
2026-05-25 13:13:27 +0530

    Update api with HTTPS domain

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://167.172.93.149/api';
+const API_URL = 'https://rmt.akva.io/api';
 
 //For Local Testing, use the following API_URL instead:
 //const API_URL = 'http://10.0.2.2:3000/api';
