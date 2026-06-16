c2f4be8ec431622e6afd9fbfd4ae1f7cad8cbcf9
Nadil-Dulran <it23161788@my.sliit.lk>
2026-06-16 15:54:22 +0530

    Update bottom navigation bar UI

--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -4,7 +4,6 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Dashboard from '../screens/main/Dashboard';
-import ProfileSettings from '../screens/profile/ProfileSettings';
 import ActivityScreen from '../screens/main/ActivityScreen';
 import GroupsScreen from '../screens/main/GroupsScreen';
 import ExpensesScreen from '../screens/main/ExpensesScreen';
@@ -15,7 +14,7 @@ const Tab = createBottomTabNavigator<BottomTabParamList>();
 
 const BottomTabs = () => {
   const insets = useSafeAreaInsets();
-  const tabBarBottomInset = Math.max(insets.bottom, 8);
+  //const tabBarBottomInset = Math.max(insets.bottom, 8);
 
   return (
     <Tab.Navigator
@@ -28,8 +27,8 @@ const BottomTabs = () => {
         tabBarStyle: [
           styles.tabBar,
           {
-            height: 68 + tabBarBottomInset,
-            paddingBottom: tabBarBottomInset,
+            height: 60 + insets.bottom,
+            paddingBottom: insets.bottom > 0 ? 8 : 6, 
           },
         ],
         tabBarItemStyle: styles.tabBarItem,
@@ -44,14 +43,6 @@ const BottomTabs = () => {
           ),
         }}
       />
-
-      <Tab.Screen
-        name="Profile"
-        component={ProfileSettings}
-        options={{
-          tabBarItemStyle: { display: 'none' }, 
-        }} 
-        />
 
        <Tab.Screen
         name="Activity"
@@ -104,12 +95,12 @@ const TabItem = ({ icon, label, focused }: any) => {
     <View style={styles.tabItem}>
       <Icon
         name={icon}
-        size={20}
+        size={23}
         color={focused ? '#15ae7e' : '#9CA3AF'}
       />
       <Text
+        allowFontScaling={false}
         numberOfLines={1}
-        adjustsFontSizeToFit
         style={[
           styles.tabLabel,
           { color: focused ? '#15ae7e' : '#9CA3AF' },
@@ -149,7 +140,8 @@ const styles = StyleSheet.create({
     borderTopColor: '#E5E7EB',
   },
   tabBarItem: {
-    flex: 1,
+     flex: 1,
+     paddingHorizontal: 0,
   },
   tabItem: {
     alignItems: 'center',
@@ -157,9 +149,11 @@ const styles = StyleSheet.create({
     marginTop: 6,
   },
   tabLabel: {
-    fontSize: 11.3,
+    width: '100%',
+    fontSize: 12,
     marginTop: 4,
     fontWeight: '600',
+    textAlign: 'center',
   },
 addButtonContainer: {
   top: -24,
@@ -168,9 +162,9 @@ addButtonContainer: {
 },
 
 addButton: {
-  width: 56,
-  height: 56,
-  borderRadius: 28,
+  width: 60,
+  height: 60,
+  borderRadius: 30,
   backgroundColor: '#15ae7e',
   justifyContent: 'center',
   alignItems: 'center',
